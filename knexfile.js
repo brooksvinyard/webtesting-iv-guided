9d591f40205be894c01b9938f795a4ef672c03a5
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-11 11:00:37 -0700

    adds pg code

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,15 @@
 // Update with your config settings.
 
+// we need to add this fake code for knex to think we have a local db
+const localPg = {
+  host: 'localhost',
+  database: 'hobbits',
+  user: 'students',
+  password: 'hired'
+}
+
+const prodDbConnection = process.env.DATABASE_URL || localPg; // we get this from Heroku
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -20,6 +30,16 @@ module.exports = {
       filename: './data/test.db3',
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: prodDbConnection, // could be an object or a string
     migrations: {
       directory: './data/migrations',
     },
